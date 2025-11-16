b83d19ef8d1e898be9cb7bd9d406963528228c6b
Dorcas Okeleji <okelejidorcas@gmail.com>
2025-11-17 01:24:04 +1300

    include the requirement

--- 4-concat.js
+++ 4-concat.js
@@ -1,3 +1,9 @@
 const args = process.argv.slice(2);
 
-console.log(args[0] + "is" + args[1]);
+if (args[1]) {
+  console.log(args[0] + "is" + args[1]);
+} else if (args[0]) {
+  console.log("HBTN");
+} else {
+  console.log("");
+}
