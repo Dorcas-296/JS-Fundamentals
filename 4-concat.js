304e978a8075f718efcaeb68167e9e3fa606ae7c
Dorcas Okeleji <okelejidorcas@gmail.com>
2025-11-17 01:32:59 +1300

    remove is from the python sentence

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 
 if (args[1]) {
-  console.log(args[0] + "is" + args[1]);
+  console.log(args[0] + " " + args[1]);
 } else if (args[0]) {
   console.log("HBTN");
 } else {
