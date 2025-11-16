6974f99cd5535a81d4c0a6f96ec7c7517eab4da9
Dorcas Okeleji <okelejidorcas@gmail.com>
2025-11-17 05:43:59 +1300

    rewrite the code

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,3 @@
 const args = process.argv.slice(2);
 
-if (args[1]) {
-  console.log(args[0] + " " + args[1]);
-} else if (args[0]) {
-  console.log("HBTN");
-} else {
-  console.log("");
-}
+console.log(args[0] + ' is ' + args[1]);
